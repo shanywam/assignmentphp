ba9155c785f973a7a71a0379e97c684c5f3fef07
shannelle <shannellenjeru@gmail.com>
2019-03-18 19:30:22 +0300

    grading assignment

---
+++ conditional.php
@@ -0,0 +1,20 @@
+<?php
+
+$grade = 70;
+
+if ($grade <= "90") {
+     echo "A!";
+   } elseif ($grade <="70") {
+     echo "A-";
+   } elseif ($grade = "60") {
+     echo "B";
+   } elseif ($grade = "50") {
+     echo "B-";
+   } elseif ($grade = "40") {
+     echo "C";
+   } elseif ($grade = "30") {
+     echo "C-";
+   } else {
+     echo "D";
+}
+?>
