844c94fa6770b0b1c2243a1202592e063968e65a
shannelle <shannellenjeru@gmail.com>
2019-03-21 16:52:55 +0300

    assignment on interface

---
+++ interface.php
@@ -0,0 +1,37 @@
+<?php
+interface Flower
+{
+    public function flowers();
+}
+interface Color
+{
+     public function colors();
+}
+
+class Rose implements Color,Flower
+{
+    public function flowers()
+    {
+        $combine=(1+1+1);
+        echo "<br>flower white rose has $combine colors in it   <br>";
+    }
+
+
+    public function colors()
+    {
+        $color=['red'=>'poppy','white'=>'lily'];
+        unset ($color['white']);
+        var_dump($color, isset($color['red']));
+        echo "color is   $color";
+    }
+    public function displayColor()
+    {
+        echo 'display color';
+    }
+
+
+}
+$rose = new Rose();
+$rose->colors();
+$rose->flowers();
+?>
