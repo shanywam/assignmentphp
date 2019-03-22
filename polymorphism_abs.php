14ba28325603791c08d9f595a2604b3932720bd3
shannelle <shannellenjeru@gmail.com>
2019-03-22 15:10:20 +0300

    updating

--- polymorphism_abs.php
+++ polymorphism_abs.php
@@ -1,9 +1,35 @@
 <?php
 abstract class User
 {
-    public $scores;
-    public $noOfArticles;
-
-    abstract public function displayScores();
+    public $noOfArticles= 20;
+    abstract public function calcScores();
 
 }
+class Author extends User
+{
+    public $noOfAuthors;
+
+    public function calcScores()
+    {
+        echo " scores are  <br><br>" . ($this->noOfArticles * 5);
+       ;
+    }
+}
+class Editor extends User
+{
+    public $noOfEditors= 2;
+     public $noOfAuthors=5;
+
+    public function calcScores()
+    {
+        echo "scores are  <br><br>". ($this->noOfArticles * $this->noOfAuthors * $this->noOfEditors);
+        ;
+    }
+
+
+}
+$author = new Author();
+$author->calcScores();
+$editor= new Editor();
+$editor->calcScores();
+?>
